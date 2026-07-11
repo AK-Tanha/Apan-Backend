b0607309148b515b3cf248ff60b4a6ae79599a15
AK Tanha <aktanha75@gmail.com>
2026-07-11 17:57:25 +0600

    chore: update root endpoint response message to Welcome To Apan Traders Backend

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome To Apan Traders Backend!"', () => {
+      expect(appController.getHello()).toBe('Welcome To Apan Traders Backend!');
     });
   });
 });
