535350785637133c95adb0de7e60236a97f56ea9
AK Tanha <aktanha75@gmail.com>
2026-07-15 18:14:27 +0600

    feat: configure CORS, global validation pipes, and app lifecycle hooks in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'https://apontraders.vercel.app',
+    ],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   const config = new DocumentBuilder()
     .setTitle('Apan Apparel API')
     .setDescription('Backend API for Court Classic / Apan Apparel storefront')
     .setVersion('1.0')
-    .addBearerAuth() // enables the "Authorize" button for JWT-protected routes later
+    .addBearerAuth()
     .build();
 
-  const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalInterceptors(new ResponseInterceptor());
 }
 bootstrap();
